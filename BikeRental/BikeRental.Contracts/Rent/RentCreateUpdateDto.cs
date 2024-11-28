9cafc2a1b9360896b1165a7b9c5c54aa9f45279c
Mavey <115408332+Matvey6M6@users.noreply.github.com>
2024-11-28 16:44:35 +0300

    Контракты апликейшн уровня

---
+++ BikeRental/BikeRental.Contracts/Rent/RentCreateUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using BikeRental.Domain.Model;
+
+namespace BikeRental.Contracts.Rent;
+
+public class RentCreateUpdateDto
+{
+    public int? BikeId { get; set; }
+
+    public int? CustomerId { get; set; }
+
+    public DateTime? Start { get; set; }
+
+    public DateTime? End { get; set; }
+}
---
+++ BikeRental/BikeRental.Contracts/Rent/RentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace BikeRental.Contracts.Rent;
+
+public class RentDto
+{
+    public int? Id { get; set; }
+
+    public int? BikeId { get; set; }
+
+    public int? CustomerId { get; set; }
+
+    public DateTime? Start { get; set; }
+
+    public DateTime? End { get; set; }
+}
